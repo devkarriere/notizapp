5dabfc1870c6198779f94f14848216822d80bfed
Frederik Diekmann <frediek@gmx.de>
2023-08-09 00:41:50 +0200

    init

---
+++ app.js
@@ -0,0 +1,119 @@
+displayNotesList();
+applyListeners();
+
+function applyListeners() {
+  const saveButtonEl = document.querySelector(".save-note");
+
+  saveButtonEl.addEventListener("click", clickSaveButton);
+
+  const noteEntriesEls = document.querySelectorAll(".note-entry");
+
+  noteEntriesEls.forEach((noteEntry) => {
+    noteEntry.addEventListener("click", () =>
+      selectNote(noteEntry.getAttribute("data-id"))
+    );
+  });
+
+  const newNoteButtonEl = document.querySelector(".create-new");
+
+  newNoteButtonEl.addEventListener("click", newNote);
+}
+
+function displayNotesList() {
+  const notes = NotesAPI.getNotes();
+
+  const sortedNotes = notes.sort(
+    (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated
+  );
+
+  let html = "";
+
+  sortedNotes.forEach((note) => {
+    html += `
+            <div class="note-entry" data-id="${note.id}">
+                <div class="note-title">${note.title}</div>
+                <div class="note-content-teaser">${note.content}</div>
+                <div class="note-date">${new Date(
+                  note.lastUpdated
+                ).toLocaleString("de-DE")}</div>
+            </div>`;
+  });
+
+  const notesListEl = document.querySelector(".notes-list");
+
+  notesListEl.innerHTML = html;
+}
+
+function clickSaveButton() {
+  const titleInputEl = document.getElementById("title-input");
+  const contentInputEl = document.getElementById("content-input");
+
+  const title = titleInputEl.value;
+  const content = contentInputEl.value;
+
+  if (!title || !content) {
+    alert("Bitte Titel und Inhalt eingeben");
+    return;
+  }
+
+  const idStoreEl = document.getElementById("note-id-store");
+
+  const currentId = idStoreEl.getAttribute("data-id");
+  console.log(currentId);
+
+  NotesAPI.saveNote(title, content, Number(currentId));
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  displayNotesList();
+  applyListeners();
+}
+
+function selectNote(id) {
+  const selectedNoteEl = document.querySelector(`.note-entry[data-id="${id}"]`);
+
+  if (selectedNoteEl.classList.contains("selected-note")) return;
+
+  removeSelectedClassFromAllNotes();
+
+  selectedNoteEl.classList.add("selected-note");
+
+  const notes = NotesAPI.getNotes();
+
+  const selectedNote = notes.find((note) => note.id === Number(id));
+
+  if (!selectedNote) return;
+
+  const titleInputEl = document.getElementById("title-input");
+  const contentInputEl = document.getElementById("content-input");
+
+  titleInputEl.value = selectedNote.title;
+  contentInputEl.value = selectedNote.content;
+
+  const idStoreEl = document.getElementById("note-id-store");
+
+  idStoreEl.setAttribute("data-id", id);
+}
+
+function removeSelectedClassFromAllNotes() {
+  const noteEntries = document.querySelectorAll(".note-entry");
+
+  noteEntries.forEach((noteEntry) => {
+    noteEntry.classList.remove("selected-note");
+  });
+}
+
+function newNote() {
+  const titleInputEl = document.getElementById("title-input");
+  const contentInputEl = document.getElementById("content-input");
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  removeSelectedClassFromAllNotes();
+
+  const idStoreEl = document.getElementById("note-id-store");
+
+  idStoreEl.removeAttribute("data-id");
+}
---
+++ notesApi.js
@@ -0,0 +1,49 @@
+const LOCAL_STORAGE_KEY = "notizapp-notizen";
+
+class NotesAPI {
+  static getNotes() {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+  }
+
+  static getNextId() {
+    const notes = this.getNotes();
+
+    const sortedNotes = notes.sort((noteA, noteB) => noteA.id - noteB.id);
+
+    let nextId = 1;
+
+    for (let note of sortedNotes) {
+      if (nextId < note.id) break;
+
+      nextId = note.id + 1;
+    }
+
+    return nextId;
+  }
+
+  static saveNote(title, content, id = undefined) {
+    const notes = this.getNotes();
+    // TODO FIX BUG
+    if (!id) {
+      notes.push({
+        title,
+        content,
+        id: this.getNextId(),
+        lastUpdated: new Date().getTime(),
+      });
+    } else {
+      const indexOfNoteWithId = notes.findIndex((note) => note.id === id);
+      console.log(indexOfNoteWithId);
+      if (indexOfNoteWithId > -1) {
+        notes[indexOfNoteWithId] = {
+          title,
+          content,
+          id,
+          lastUpdated: new Date().getTime(),
+        };
+      }
+    }
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+  }
+}
